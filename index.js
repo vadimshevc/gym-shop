243b076b15fe7b34fb8c17d3b953a14e394d7367
SonOfPerune <shevcenko123465@gmail.com>
2023-08-06 12:16:11 +0300

    Third commit

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function render() {
 
 }
 
-render();
-
 let CATALOG = [];
+//https://api.jsonserve.com/7kiyJI
+//server/catalog.json
+fetch('server/catalog.json')
+    .then(res => res.json())
+    .then(body => {
+        CATALOG = body;
+        render();
+    })
+    .catch(eror => {
+        console.log(eror);
+    });
