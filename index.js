658b3273e80a436a9cc1268ecdcddf108d44916f
SonOfPerune <shevcenko123465@gmail.com>
2023-08-06 12:38:07 +0300

    Spinner

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ function render() {
 
 }
 
+spinnerPage.render();
+
 let CATALOG = [];
 //https://api.jsonserve.com/7kiyJI
 //server/catalog.json
@@ -13,8 +15,16 @@ fetch('server/catalog.json')
     .then(res => res.json())
     .then(body => {
         CATALOG = body;
+
+       /* setTimeout(() =>  {
+            spinnerPage.handelClear();
+            render();
+        }, 1000);*/
+
+        spinnerPage.handelClear();
         render();
     })
     .catch(eror => {
         console.log(eror);
     });
+
